144fa96af416ff386089fbbf5f10ceda043e5620
Naresh28Babu <naresh-2182@naresh-2182.csez.zohocorpin.com>
2017-08-17 04:19:40 +0530

    stats modified for zoho books data

---
+++ js/apiHelper.js
@@ -0,0 +1,224 @@
+APIHelper = (function()
+		{
+	var booksApiKey  = undefined;
+	var widgetLoadData = undefined;
+	function mergeArray(obj){
+		var temp = {};
+		jQuery.each(obj,function(index,obj){
+
+			temp = jQuery.extend(temp,obj);
+		});
+		return temp;
+	}
+	var statusToCheck = {
+			PurchaseOrder : "billed",
+			SalesOrder : "open",
+	}
+
+	return {
+		init : function(onLoadData)
+		{
+			if(onLoadData && onLoadData.LineItems && onLoadData.LineItems.length > 0)
+			{
+				return ZOHO.CRM.CONFIG.getOrgVariable(Utils.cons.authKeyField)
+				.then(function(data){
+					booksApiKey = data.Success.Content;
+					widgetLoadData = onLoadData;
+					return onLoadData;
+				});
+			}
+			return Promise.reject({
+				err : "NO_LINE_ITEM"
+			});
+
+		},
+		getProductFromCRM : function(context)
+		{
+			return ZOHO.CRM.API.getRecord({Entity:"Products",RecordID:context.LineItem.ID})
+			.then(function(data){
+				context.ProdInfo = data;
+				return context;
+			});
+
+		},
+		getPOFromBooks : function(context)
+		{
+			var req = {
+					url : "https://books.zoho.com/api/v3/purchaseorders",
+					params:{
+						item_id:context.ItemInfo.item_id,
+						authtoken:booksApiKey
+					}
+			};
+			return ZOHO.CRM.HTTP.get(req)
+			.then(function(response){
+				var response = JSON.parse(response);
+				if(response && response.message === 'success' && response.purchaseorders && response.purchaseorders.length > 0){
+					context.POLists = response.purchaseorders
+				}
+				return context;
+			});
+		},
+		getSOFromZBooks : function(context)
+		{
+			var req = {
+					url : "https://books.zoho.com/api/v3/salesorders",
+					params:{
+						item_id:context.ItemInfo.item_id,
+						authtoken:booksApiKey
+					}
+			};
+			return ZOHO.CRM.HTTP.get(req)
+			.then(function(response){
+				var response = JSON.parse(response);
+				if(response && response.message === 'success' && response.salesorders && response.salesorders.length > 0){
+					context.SOLists = response.salesorders
+				}
+				return context;
+			});
+		},
+		getItemFromZBooks : function(context)
+		{
+			var req = {
+					url : "https://books.zoho.com/api/v3/items",
+					params:{
+						name:context.LineItem.Name,
+						authtoken:booksApiKey
+					}
+			};
+
+			return ZOHO.CRM.HTTP.get(req)
+			.then(function(response){
+				var response = JSON.parse(response);
+				if(response && response.message === 'success' && response.items && response.items.length > 0){
+					context.ItemInfo = response.items[0]
+				}
+				return context;
+			})
+		},
+		getCompleteProdInfo : function(itemInfo)
+		{
+			/*
+			 * fetch item info from books
+			 */
+			var itemPromise = APIHelper.getItemFromZBooks({LineItem:itemInfo})
+			/*
+			 * fetch SO and PO after fetching itemData
+			 */
+			var SOPromise = itemPromise
+			.then(APIHelper.getSOFromZBooks);
+			var POPromise = itemPromise
+			.then(APIHelper.getPOFromBooks);
+			/*
+			 * Combine SO and PO data together
+			 */
+			var booksData = Promise.all([SOPromise,POPromise])
+			.then(function(data){
+					return mergeArray(data); 
+				});
+
+			/*
+			 * Fetch ProductInfo from CRM
+			 * To Extract Vendor Info
+			 */
+			var crmData = APIHelper.getProductFromCRM({LineItem:itemInfo});
+
+			/*
+			 * Merge Crm and Books data and return the promise
+			 */
+			return Promise.all([booksData,crmData])
+			.then(function(data){
+				return mergeArray(data);
+			})
+		},
+		computeDataForStats : function(allData){
+			/*
+			 * Computing stock in hand
+			 */
+			var stockInHand = 0;
+			if(allData.ItemInfo){
+				stockInHand = allData.ItemInfo.stock_on_hand;
+			}
+
+			/*
+			 * Computing quantity Ordered
+			 */
+			var stockOrdered = 0;
+			if(allData.POLists && allData.POLists.length > 0 ){
+
+				jQuery.each(allData.POLists,function(index,obj){
+
+					if(obj && obj.status === statusToCheck.PurchaseOrder){
+						stockOrdered += obj.item_quantity
+					}
+				});
+			}
+
+			/*
+			 * Computing quantity already in demand
+			 */
+			var stockInDemand = 0;
+			if(allData.SOLists && allData.SOLists.length > 0 ){
+
+				jQuery.each(allData.POLists,function(index,obj){
+
+					if(obj && obj.status === statusToCheck.SalesOrder){
+						stockInDemand += obj.item_quantity
+					}
+				});
+			}
+			var qtyRequired =allData.LineItem.NewQuantity
+			var adjusment = (stockInHand+stockOrdered) - (stockInDemand+qtyRequired);
+			allData.Stats = {
+					stockInHand : stockInHand,
+					stockOrdered : stockOrdered,
+					stockInDemand : stockInDemand,
+					adjusment : adjusment,
+					purchase : (adjusment < 0 ? Math.abs(adjusment) : 0),
+					isPurchaseRequired : (adjusment < 0 ? true : false)
+			}
+			return allData;
+		},
+
+		generateStats : function(itemInfo){
+			
+			return APIHelper.getCompleteProdInfo(itemInfo)
+			.then(function(data){
+				return APIHelper.computeDataForStats(data);
+			})
+		},
+		createPurchaseOrder : function(){
+
+			var sub = $("#POSubject").val();
+			var vendor = $("#POVendor").val();
+
+			var products = [];
+			$("input:checked").each(function(ind,obj){
+
+				var productID = $(obj).attr("name");
+				var qty = $("#"+productID+"_Qty").val();
+				qty = parseInt(qty)
+				var temp = {
+					product:{
+						id : productID
+					},
+					quantity : qty
+				}
+				products.push(temp);
+			})
+			var recordData = {
+			        "Subject": sub,
+					"Vendor_Name":vendor,
+					"Product_Details":products,
+					"Status":"Created"
+			  }
+			ZOHO.CRM.API.insertRecord({Entity:"Purchase_Orders",APIData:recordData}).then(function(data){
+
+				if(data && data[0] && data[0].code === 'SUCCESS')
+				{
+					Handler.widgetInit(widgetLoadData);
+				}
+			});
+		}
+	}
+		})()
--- js/handler.js
+++ js/handler.js
@@ -1,9 +1,23 @@
+Handlebars.registerHelper('isPurchaseRequired', function(items,options) {
+	    for(var i=0;i<items.length;i++){
+    	var temp = items[i];
+    	if(temp.isPurchaseRequired){
+    		return options.fn(this);
+    	}
+    }
+    return options.inverse(this);
+});
+Handlebars.registerHelper("inc", function(value, options)
+{
+    return parseInt(value) + 1;
+});
+
 /*
  * util methods
  */
 Utils={
 	cons:{
-		authKey : "testplugin.booksAuth"
+		authKeyField : "zcrmbooksinteg.booksAuth"
 	},
 	meta:{
 		token : undefined
@@ -38,169 +52,61 @@ Utils.genHtml=function(templateId,data,callBack){
 };
 Utils.closePopUp = function(){
 	ZOHO.CRM.UI.Popup.close()
-}
-
-var Handler = {};
-Handler.getItemFromZCrm = function(item)
-{
-	return ZOHO.CRM.API.getRecord({Entity:"Products",RecordID:item.ID});
-}
-
-Handler.getItemFromZBooks = function(item)
-{
-    var req = {
-      url : "https://books.zoho.com/api/v3/items",
-      params:{
-        name:item.Name,
-        authtoken:Utils.meta.token
-      }
-    };
-  return ZOHO.CRM.HTTP.get(req)
-  .then(function(response){
-    var response = JSON.parse(response);
-    if(response && response.message === 'success' && response.items && response.items.length > 0){
-      return response.items[0];
-    }
-  });
-}
-Handler.getItemData = function(item)
-{
-	var consolidatedData = {};
-	
-	var crmData = Handler.getItemFromZCrm(item)
-	.then(function(data){
-		consolidatedData.CrmData = data
-	});
-
-	var DeskData = Handler.getItemFromZBooks(item)
-	.then(function(data){
-		consolidatedData.DeskData = data
-	});
-
-	consolidatedData.LineItem = item;
-
-	return Promise.all([crmData,DeskData])
-	.then(function(){
-		
-		var stockInHand = 0;
-		if(consolidatedData.DeskData){
-			stockInHand = consolidatedData.DeskData.stock_on_hand;	
-		}
-		var stockOrdered = consolidatedData.CrmData.Qty_Ordered;
-		var oldDemain = consolidatedData.CrmData.Qty_in_Demand;
-		var newDemain = parseInt(consolidatedData.LineItem.NewQuantity);
-		var adjusment = (stockInHand+stockOrdered) - (oldDemain+newDemain);
-		consolidatedData.Calculation = {
-			adjusment :  adjusment,
-			purchase  : (adjusment < 0 ? Math.abs(adjusment) : 0)
-		}
-		consolidatedData.isPurchaseRequired = adjusment < 0 ? true : false
-
-		return consolidatedData;
-	});
-}
-var widget = {
-	onLoadData : undefined
 };
-Handler.widgetInit = function(data)
-{
-	ZOHO.CRM.CONFIG.getOrgVariable(Utils.cons.authKey)
-	.then(function(data){
-		if(data && data.SUCCESS){
-			Utils.meta.token = data.SUCCESS;
-		}
-	})
-	.then(function(){
-		  widget.onLoadData = data;
-		  if(data && data.LineItems)
-		  {
-		    var lineItems = data.LineItems;
-		    if(lineItems)
-		    {
-		      var itemDetails = [];
-		      lineItems.forEach(function(item)
-		      {
-		      
-		          itemDetails.push(Handler.getItemData(item));
-		      
-		      });
-		      Promise.all(itemDetails)
-		      .then(function(data){
-		      	var vendor = undefined;
-		      	$.each(data,function(index,obj){
-		      		if(obj.CrmData.Vendor_Name){
-		      			vendor = obj.CrmData.Vendor_Name;
-		      		}
-		      	});
 
-		        console.log(data);
-		        Utils.RenderTemplate("stockReport",{
-		      		data:data,
-		      		vendor : vendor
-		      	});
-		      });
-		    }
-		  }
-		  else{
-		  	Utils.closePopUp();
-		    alert("No line Item");
-		  }
-	});
-}
-// HandleBar Helpers
-Handlebars.registerHelper('isPurchaseRequired', function(items,options) {
-	    for(var i=0;i<items.length;i++){
-    	var temp = items[i];
-    	if(temp.isPurchaseRequired){
-    		return options.fn(this);
-    	}
-    }
-    return options.inverse(this);
-});
 
-Handlebars.registerHelper("inc", function(value, options)
-{
-    return parseInt(value) + 1;
-});
-
-widget.ShowPurchaseOrder = function(){
+Utils.ShowPurchaseOrder = function(){
 	$(".reportTab").hide();
 	$(".purchaseOrderCreation").show();
 }
-widget.ShowSalesOrder = function(){
-	$(".reportTab").show();
-	$(".purchaseOrderCreation").hide();
-}
-widget.createPurchaseOrder = function(){
 
-	var sub = $("#POSubject").val();
-	var vendor = $("#POVendor").val();
+/*
+ * Main Bussiness logic
+ */
+var Handler = {};
+Handler.widgetInit = function(data)
+{
+	APIHelper.init(data)
+	.then(function(pageContent){
 
-	var products = [];
-	$("input:checked").each(function(ind,obj){
-
-		var productID = $(obj).attr("name");
-		var qty = $("#"+productID+"_Qty").val();
-		qty = parseInt(qty)
-		var temp = {
-			product:{
-				id : productID
-			},
-			quantity : qty
-		}
-		products.push(temp);
+		var allItemsPromise = [];
+		jQuery.each(pageContent.LineItems,function(index,obj){
+			allItemsPromise.push(APIHelper.generateStats(obj));
+		});
+		return Promise.all(allItemsPromise);
 	})
-	var recordData = {
-	        "Subject": sub,
-			"Vendor_Name":vendor,
-			"Product_Details":products,
-			"Status":"Created"
-	  }
-	ZOHO.CRM.API.insertRecord({Entity:"Purchase_Orders",APIData:recordData}).then(function(data){
-
-		if(data && data[0] && data[0].code === 'SUCCESS')
-		{
-			Handler.widgetInit(widget.onLoadData);
+	.then(function(allData){
+		/*
+		 * find atleast one vendor to pre populate
+		 */
+      	var vendor = undefined;
+      	var isPurchaseRequired = false;;
+      	$.each(allData,function(index,obj){
+      		if(obj.ProdInfo.Vendor_Name){
+      			vendor = obj.ProdInfo.Vendor_Name;
+      		}
+      		if(obj.Stats.isPurchaseRequired){
+      			isPurchaseRequired = true;
+      		}
+      	});
+      	
+      	return {
+      		data : allData,
+      		vendor : vendor,
+      		purchaseRequired : isPurchaseRequired
+      	}
+	})
+	.then(function(data){
+		console.log(data);
+		Utils.RenderTemplate("stockReport",data);
+	})
+	.catch(function(data)
+	{
+		if(data && data.err === 'NO_LINE_ITEM'){
+			Utils.closePopUp();
+		    alert("No line Item");
 		}
+	  	
 	});
+	
 }
